b6a5cc4620fe47e36425e7301154d20756ae8b08
Robin <67558040+midouest@users.noreply.github.com>
2025-03-15 17:07:53 +0000

    octave led behavior

--- lib/grid/keyboard.lua
+++ lib/grid/keyboard.lua
@@ -34,10 +34,10 @@ function Keyboard:key(x, y, s)
       elseif y == 8 then
         self.octave = math.max(self.octave - 1, 0)
       elseif y ~= 5 then
-        self.value = self.octave * 12 + PATTERN[y][1]
+        self.value = util.clamp(self.octave * 12 + PATTERN[y][1], 0, 127)
       end
     else
-      self.value = self.octave * 12 + PATTERN[y][2]
+      self.value = util.clamp(self.octave * 12 + PATTERN[y][2], 0, 127)
     end
     if self.value ~= prev_value and self.action then
       self.action(self.value)
@@ -50,8 +50,14 @@ function Keyboard:redraw(g)
   if self.hidden then
     return
   end
+  if self.octave > 5 then
+    g:led(self.x, 1, (self.octave - 5) * 3)
+  elseif self.octave < 5 then
+    g:led(self.x, 8, (5 - self.octave) * 3)
+  end
   for x = 1, 2 do
-    for y = 1, 8 do
+    local y0 = self.octave == 10 and 4 or 1
+    for y = y0, 8 do
       if PATTERN[y][x] >= 0 then
         g:led(x + self.x - 1, y, 3)
       end
